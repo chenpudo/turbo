350ca8e0beca343ab41006876e4fd53386aaf76d
chenpudo <chenpudo@gmail.com>
2019-03-25 20:10:11 -0400

    make update to guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,28 +1,23 @@
-echo "please guess and enter the number of files in current directory"
+echo "how many files are there in current directory?"
 read number
 
-if [[ $number -lt 12 ]]
+function guessfile(){
+
+if [[ $guess -lt $number ]]
  then 
-  while [[ $number -gt 0 ]] && [[ $number -lt 12 ]] 
-    do
-     echo  "your guess is lower than correct number, please enter again"
-       read number
-       let number=$number-1
-   done      
-      
-
-
-elif [[ $number -gt 12 ]]
-  then 
-   while [[ $number -gt 0 ]] && [[ $number -gt 12 ]]
-     do 
-        echo "your guess is higher than correct number, please enter again"
-        read number
-        let number=$number-12
-      done
-
-else [[ $number -eq 12 ]] 
- echo "congrats, you got it right"
-
+  echo "your guess is lower than correct number, guess again"
+ elif [[ $guess -gt $number ]]
+ then 
+ echo "your guess is higher than correct number, guess again"
 fi
+}
 
+echo "guess how many files in current directory?"
+
+
+while  [[ $guess -ne $number ]]
+  do 
+   read guess
+    guessfile
+  done
+echo  "congrats, you got it right"
